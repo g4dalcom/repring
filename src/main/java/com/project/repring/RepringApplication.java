3ed1d2bb2ea9dc7ccae5450c63c512e9ea09fd9c
g4dalcom <g4dalcom@gmail.com>
2022-12-16 03:36:24 +0900

    initial commit

---
+++ src/main/java/com/project/repring/RepringApplication.java
@@ -0,0 +1,13 @@
+package com.project.repring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/project/repring/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.project.repring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String Test() {
+        return "success!";
+    }
+}
