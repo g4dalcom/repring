1fbab1eb3dd8dc63dac7f9f1bdd0f17527b3ba55
g4dalcom <g4dalcom@gmail.com>
2023-06-19 19:30:43 +0900

    git reset

--- src/main/frontend/src/component/AddPost.jsx
+++ src/main/frontend/src/component/AddPost.jsx
@@ -1,33 +1,32 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Modal from "./Modal";
 import styled from "styled-components";
 
 const AddPost = () => {
+  const [modalUp, setModalUp] = useState(false);
 
-    const [modalUp, setModalUp] = useState(false);
+  const showModal = () => {
+    setModalUp(true);
+  };
 
-    const showModal = () => {
-        setModalUp(true);
-    };
-
-    return (
-        <StEmptyCard>
-            <button onClick={showModal}>추가하기</button>
-            {modalUp && <Modal setModalUp={setModalUp} />}
-        </StEmptyCard>
-    )
-}
+  return (
+    <StEmptyCard>
+      <button onClick={showModal}>추가하기</button>
+      {modalUp && <Modal setModalUp={setModalUp} />}
+    </StEmptyCard>
+  );
+};
 
 export default AddPost;
 
 const StEmptyCard = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100px;
-    height: 100px;
-    border: 3px solid purple;
-    border-radius: 10px;
-    padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  border: 3px solid purple;
+  border-radius: 10px;
+  padding: 10px;
 `;
